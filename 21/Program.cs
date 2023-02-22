468e2297127bb0dcaa30032de16da054c56b3ef4
Anna Nalimova <avnalimova@mail.ru>
2023-02-22 14:20:08 +0300

    21

---
+++ 21/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите x1 ");
+double x1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите y1 ");
+double y1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите z1 ");
+double z1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите x2 ");
+double x2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите y2 ");
+double y2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите z2");
+double z2 = double.Parse(Console.ReadLine());
+double s =Math.Sqrt(Math.Pow(x1-x2,2)+Math.Pow(y1-y2,2)+ Math.Pow(z1-z2,2));
+Console.WriteLine($"s={s:f3}");
