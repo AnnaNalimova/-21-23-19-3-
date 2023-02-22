1526529744e049edb583a86dcaef03ca61695680
Anna Nalimova <avnalimova@mail.ru>
2023-02-22 15:33:18 +0300

    23

---
+++ 23/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая  принимает на вход число N и выдает таблицу кубов чисел от 1 до N
+Console.WriteLine("Введите  число N : ");
+int N = int.Parse(Console.ReadLine());
+int[]array = new int[N];
+for(int i=1;i<=N;i++)
+  array[i-1] =i * i * i;
+for(int i=0;i<N;i++)
+  Console.Write($"{array[i]},");
+
+
